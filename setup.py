b1a641a1db29b2987851faa30a2c24c3fc5e638c
Chad Dotson <chad@cdotson.com>
2016-01-03 12:08:45 -0600

    Add more metadata fields.

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ with open("requirements.txt", "r'") as f:
     install_reqs = f.readlines()
 
 setup(name='pyradar',
+      author="Chad Dotson",
+      author_email="chad@cdotson.com",
+      url="http://www.cdotson.com",
       version='1.0',
       packages=['PyRadar', 'scripts'],
       install_requires=install_reqs,
