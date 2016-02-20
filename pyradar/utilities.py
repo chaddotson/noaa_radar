698cdf76a8517e6a389155db40d73f170d3f96bc
Chad Dotson <chad@cdotson.com>
2016-02-20 14:36:08 -0600

    Update code around urllib2 because of python 3.

--- pyradar/utilities.py
+++ pyradar/utilities.py
@@ -1,13 +1,16 @@
-__author__ = 'Chad Dotson'
-
 from six.moves import cStringIO
 
-import urllib2
+try:
+    from urllib2 import build_opener
+except ImportError:
+    from urllib.request import build_opener
 
 from PIL import Image
 
+__author__ = 'Chad Dotson'
+
 def get_image_from_url(url):
-    opener = urllib2.build_opener()
+    opener = build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)')]
     response = opener.open(url)
     im = cStringIO.StringIO(response.read())
