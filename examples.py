9c0a8a9f3eef19df8c3604afd62b5dd8e30e9f83
Chad Dotson <chad@cdotson.com>
2016-02-20 14:04:26 -0600

    Uncomment example code.

--- examples.py
+++ examples.py
@@ -1,20 +1,21 @@
+from pyradar import NOAA
 
-#noaa = NOAA()
-#
-#image = noaa.get_composite_reflectivity('HTX')
-#image.save("composite_reflectivity.jpg")
-#
-#image = noaa.get_base_reflectivity('HTX')
-#image.save("base_reflectivity.jpg")
-#
-#image = noaa.get_storm_relative_motion('HTX')
-#image.save("storm_relative_motion.jpg")
-#
-#image = noaa.get_base_velocity('HTX')
-#image.save("base_velocity.jpg")
-#
-#image = noaa.get_one_hour_precipitation('HTX')
-#image.save("one_hour_precipitation.jpg")
-#
-#image = noaa.get_storm_total_precipitation('HTX')
-#image.save("storm_total_precipitation.jpg")
+noaa = NOAA()
+
+image = noaa.get_composite_reflectivity('HTX')
+image.save("composite_reflectivity.jpg")
+
+image = noaa.get_base_reflectivity('HTX')
+image.save("base_reflectivity.jpg")
+
+image = noaa.get_storm_relative_motion('HTX')
+image.save("storm_relative_motion.jpg")
+
+image = noaa.get_base_velocity('HTX')
+image.save("base_velocity.jpg")
+
+image = noaa.get_one_hour_precipitation('HTX')
+image.save("one_hour_precipitation.jpg")
+
+image = noaa.get_storm_total_precipitation('HTX')
+image.save("storm_total_precipitation.jpg")
