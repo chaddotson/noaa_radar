16df05cd87a54d2654368f6776abe0fcc3485974
Chad Dotson <chad@cdotson.com>
2016-02-20 14:37:29 -0600

    Another fix for using six.

--- pyradar/utilities.py
+++ pyradar/utilities.py
@@ -13,5 +13,5 @@ def get_image_from_url(url):
     opener = build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)')]
     response = opener.open(url)
-    im = cStringIO.StringIO(response.read())
+    im = cStringIO(response.read())
     return Image.open(im)
